77c1ceaccdf31d4aca2b4ff6051bb071dc30ceb4
David Polcari <dpolcari18@gmail.com>
2021-05-24 17:21:09 -0500

    nav bar active working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 import Home from './containers/Home'
 
 function App() {
+
   return (
     <Router>
-      <Route path='/resume' component={Home} />
-      <Route path='/contact' component={Home} />
-      <Route path='/blog' component={Home} />
-      <Route path='/portfolio' component={Home} />
-      <Route path='/about' component={Home} />
-      <Route path='/' component={Home} />
+      <Route path='/resume' 
+             component={Home} 
+             onEnter={() => document.getElementById('resume_id').scrollIntoView()}
+      />
+      <Route path='/contact' 
+             component={Home} 
+             onEnter={() => document.getElementById('contact_id').scrollIntoView()}
+      />
+      <Route path='/blog' 
+             component={Home} 
+             onEnter={() => document.getElementById('blog_id').scrollIntoView()}
+      />
+      <Route path='/portfolio' 
+             component={Home} 
+             onEnter={() => document.getElementById('portfolio_id').scrollIntoView()}
+      />
+      <Route path='/about' 
+             component={Home} 
+             onEnter={() => document.getElementById('about_id').scrollIntoView()}
+      />
+      <Route path='/' 
+             component={Home} 
+             onEnter={() => document.getElementById('home_id').scrollIntoView()}
+      />
     </Router>
   );
 }
--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// semantic ui
+import { Menu } from 'semantic-ui-react'
 
 const NavBar = () => {
+
+    const [active, setActive] = useState('Home')
+
+    const handleClick = (e) => {
+        setActive(e.target.innerText)
+    }
+
     return (
-        <div>
-            Navbar
-        </div>
+        <Menu secondary>
+            <Menu.Item 
+                name='Home'
+                active={active === 'Home'}
+                onClick={(e) => handleClick(e)}
+            />
+            <Menu.Item 
+                name='About'
+                active={active === 'About'}
+                onClick={(e) => handleClick(e)}
+            />
+            <Menu.Item 
+                name='Portfolio'
+                active={active === 'Portfolio'}
+                onClick={(e) => handleClick(e)}
+            />
+            <Menu.Item 
+                name='Blog'
+                active={active === 'Blog'}
+                onClick={(e) => handleClick(e)}
+            />
+            <Menu.Item 
+                name='Contact'
+                active={active === 'Contact'}
+                onClick={(e) => handleClick(e)}
+            />
+            <Menu.Item 
+                name='Resume'
+                active={active === 'Resume'}
+                onClick={(e) => handleClick(e)}
+            />
+        </Menu>
     )
 }
 
