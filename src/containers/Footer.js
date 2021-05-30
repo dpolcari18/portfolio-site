35dcd9928bd5c44e3913fae526e09ed09c5f9eae
David Polcari <dpolcari18@gmail.com>
2021-05-30 11:26:31 -0500

    centered footer and changed default link color

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -6,10 +6,10 @@ import { Menu, Icon } from 'semantic-ui-react'
 const Footer = () => {
     return (
         <Menu fixed='bottom' id='footer'>
-            <Icon name='github square' size='huge'/>
-            <Icon name='linkedin' size='huge'/>
-            <Icon name='medium' size='huge' />
-            <Icon name='mail' size='huge' />
+            <a href='https://github.com/dpolcari18' target='_blank'><Icon name='github square' size='huge'/></a>
+            <a href='https://www.linkedin.com/in/david-polcari/' target='_blank'><Icon name='linkedin' size='huge'/></a>
+            <a href='https://davidpolcari.medium.com/' target='_blank'><Icon name='medium' size='huge' /></a>
+            <a href='mailto:dpolcari18@gmail.com'><Icon name='mail square' size='huge' /></a>
         </Menu>
     )
 }
