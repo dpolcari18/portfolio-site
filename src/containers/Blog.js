596dd8181b45f00dd8237537b67dfc5501f5534f
David Polcari <dpolcari18@gmail.com>
2021-06-02 16:31:47 -0500

    removed blog links

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,20 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 // components
 import BlogPost from '../components/BlogPost'
 
-// fetch blog posts
-import { getFetchCall } from '../functions/Functions'
-
-// endpoint for medium blog
-const URL = 'https://medium.com/feed/@davidpolcari'
 
 const Blog = () => {
-
-    // fetch blog posts
-    useEffect(() => {
-        const blogsRes = getFetchCall(URL)
-    }, [])
 
     return (
         <div className='view' id='blog_id'>
