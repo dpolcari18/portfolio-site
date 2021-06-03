c60cfeb435c763c3666eef550744dda823e779e0
David Polcari <dpolcari18@gmail.com>
2021-06-03 17:41:36 -0500

    controlled form created

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+// react-semantic
+import { Form, Grid } from 'semantic-ui-react'
 
 const Contact = () => {
+
+    // local state
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [body, setBody] = useState('')
+
     return (
-        <div className='view' id='contact_id'>
-            Contact
-        </div>
+        <Grid className='view align_view' id='contact_id'>
+            <Grid.Row>
+                <Grid.Column width='5'>
+
+                </Grid.Column>
+                <Grid.Column width='5'>            
+                    <Form widths='equal'>
+                        <Form.Input fluid 
+                                    label='Email' 
+                                    placeholder='Your Email Here' 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}/>
+                        <Form.Input fluid 
+                                    label='Subject' 
+                                    placeholder='Subject Line' 
+                                    value={subject}
+                                    onChange={(e) => setSubject(e.target.value)}/>
+                        <Form.TextArea fluid 
+                                    label='Body' 
+                                    placeholder='What do you want to say?' 
+                                    value={body}
+                                    onChange={(e) => setBody(e.target.value)}/>
+                        <Form.Button>Send</Form.Button>
+                    </Form>
+                </Grid.Column>        
+            </Grid.Row>
+        </Grid>
     )
 }
 
