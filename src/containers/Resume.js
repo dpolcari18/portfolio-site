acdc0bca424a23aebbebb6072c63eb5784f19eb0
David Polcari <dpolcari18@gmail.com>
2021-06-02 21:25:31 -0500

    got pdf showing with downloadable link

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+// react-pdf
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+
+// react semantic
+import { Grid } from 'semantic-ui-react'
+
+import file from '../content/resume.pdf'
+
 const Resume = () => {
     return (
-        <div className='view' id='resume_id'>
-            Resume
-        </div>
+        <Grid className='view align_view' id='resume_id'>
+            <a href={file} download>
+                <Document file={file} >
+                    <Page pageNumber={1} height={750}/>
+                </Document>
+            </a>
+        </Grid>
     )
 }
 
